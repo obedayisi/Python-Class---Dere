4d0f8f1bf31498768ad57cb7c540db1f64672801
Obed Ayisi <oaayisi@gmail.com>
2023-02-06 14:00:31 -0500

    This is my first commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+# hello.py    #prints the string "Hello World" to the console
+
+print("Hello World\n")
+print(5 + 5)
---
+++ simpleprog.py
@@ -0,0 +1,7 @@
+#create a program that asks a user for a number and then cubes items
+
+answer = input("Please enter a number to cube: ")
+
+number = int(answer)
+
+print("Your number cubed is:", number ** 3)
